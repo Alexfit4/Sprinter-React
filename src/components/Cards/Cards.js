88677aebec4c84b0e37666caae7ddfe64284348d
johnniesimpson <johnniesimpson@outlook.com>
2021-04-10 11:59:02 -0400

    Created card component and added to Dashboard component.

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+// const title = ["Summary", "Managers", "Employees", "Timeline"]
+
+export default (props) => {
+    return (
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>{props.title}</Card.Title>
+    <Card.Text>
+        {props.text}
+    </Card.Text>
+  </Card.Body>
+</Card>
+    )
+}
