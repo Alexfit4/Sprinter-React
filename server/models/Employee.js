60f46e64d8f5f046f19aaeeb434634b3dc5632e1
alexfit4 <alex.ashtiany@gmail.com>
2021-04-10 21:36:41 -0400

    added roles to employee model, and also rendered data to front end

--- server/models/Employee.js
+++ server/models/Employee.js
@@ -1,18 +1,36 @@
 import mongoose from "mongoose";
 
-const employeeSchema = mongoose.Schema({
-	first_name:{
-        type: String
-    },
-	last_name:{
-        type: String
-    },
-	createdAt: {
-		type: Date,
-		default: new Date(),
+const employeeSchema = mongoose.Schema(
+	{
+		first_name: {
+			type: String,
+		},
+		last_name: {
+			type: String,
+		},
+		role: [
+			{
+				title: {
+					type: String,
+				},
+				salary: {
+					type: Number,
+				},
+			},
+		],
+		createdAt: {
+			type: Date,
+			default: new Date(),
+		},
 	},
-});
+	{
+		toJSON: {
+			// include any virtual properties when data is requested
+			virtuals: true,
+		},
+	}
+);
 
-const EmployeeSchema = mongoose.model('EmployeeSchema', employeeSchema)
+const EmployeeSchema = mongoose.model("EmployeeSchema", employeeSchema);
 
-export default EmployeeSchema
+export default EmployeeSchema;
