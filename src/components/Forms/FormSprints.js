27ed12da490f2e31797d99f4c61d8495a14054bf
Lu Hao <haolu1125@gmail.com>
2021-04-17 14:30:37 -0400

    multi selection

--- src/components/Forms/FormSprints.js
+++ src/components/Forms/FormSprints.js
@@ -2,27 +2,37 @@ import React, { useState, useEffect, Component } from "react";
 import { Form, Button, Col, Container, h1, InputGroup, Row } from "react-bootstrap";
 import axios from "axios";
 import { Multiselect } from 'multiselect-react-dropdown';
+import Select from "react-select";
 
 const SprintsForms = (props) => {
+
+
+
     // export default class SprintsForms extends Component {
 
-    this.onChangeTitle = this.onChangeTitle.bind(this);
-    this.onChangeDescription = this.onChangeDescription.bind(this);
-    this.onChangeStartDate = this.onChangeStartDate.bind(this);
-    this.onChangeManager = this.onChangeManager.bind(this);
-    this.onChangeEmployee = this.onChangeEmployee.bind(this);
-    this.onChangeStatus = this.onChangeStatus.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
+    // this.onChangeTitle = this.onChangeTitle.bind(this);
+    // this.onChangeDescription = this.onChangeDescription.bind(this);
+    // this.onChangeStartDate = this.onChangeStartDate.bind(this);
+    // this.onChangeManager = this.onChangeManager.bind(this);
+    // this.onChangeEmployee = this.onChangeEmployee.bind(this);
+    // this.onChangeStatus = this.onChangeStatus.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
 
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [startDate, setstartDate] = useState(new Date());
     const [manager, setManager] = useState([]);
     const [employee, setEmployee] = useState([]);
-    const [status, setStatus] = useState("");
+    const [status, setStatus] = useState(['OPEN', 'IN PROGRESS', "IN REVIEW", "DONE"]);
     const [employeeData, setEmployeeData] = useState([]);
+    const [selectedEmployee, setSelectedEmployee] = useState("none");
+
+    const handleTypeSelect = e => {
+        setSelectedEmployee(e.value);
+    };
 
 
+    // const status = ['open', 'in progress', "in review", "done"]
 
 
 
@@ -79,59 +89,63 @@ const SprintsForms = (props) => {
     //         })
     // }
 
+    // const optionTest = [
+    //     { value: 'chocolate', label: 'Chocolate' },
+    //     { value: 'strawberry', label: 'Strawberry' },
+    //     { value: 'vanilla', label: 'Vanilla' }
+    // ]
+
     useEffect(() => {
         axios.get('http://localhost:5000/employee')
-            .then(response => {
-                if (response.data.length > 0) {
-                    this.setState({
-                        employee: response.data.map(EmployeeSchema => `${EmployeeSchema.first_name} ${EmployeeSchema.last_name}`),
-                    })
-                }
-                console.log(this.state.employee)
+            .then(res => {
+                setEmployee(res.data.map(EmployeeSchema => `${EmployeeSchema.first_name} ${EmployeeSchema.last_name}`)
+                )
             })
-
             .catch((error) => {
                 console.log(error);
             })
+
     }, [])
 
-    status = ['open', 'in progress', "in review", "done"]
 
-    onChangeTitle = (e) => {
-        this.setState({
-            title: e.target.value
-        })
-    }
 
-    onChangeDescription = (e) => {
-        this.setState({
-            description: e.target.value
-        })
-    }
+    // onChangeTitle = (e) => {
+    //     this.setState({
+    //         title: e.target.value
+    //     })
+    // }
 
-    onChangeStartDate = (date) => {
-        this.setState({
-            startDate: date
-        })
-    }
+    // onChangeDescription = (e) => {
+    //     this.setState({
+    //         description: e.target.value
+    //     })
+    // }
 
-    onChangeManager = (e) => {
-        this.setState({
-            manager: e.target.value
-        })
-    }
+    // onChangeStartDate = (date) => {
+    //     this.setState({
+    //         startDate: date
+    //     })
+    // }
 
-    onChangeEmployee = (e) => {
-        this.setState({
-            employee: e.target.value
-        })
-    }
+    // onChangeManager = (e) => {
+    //     this.setState({
+    //         manager: e.target.value
+    //     })
+    // }
 
-    onChangeStatus = (e) => {
-        this.setState({
-            status: e.target.value
-        })
-    }
+    // onChangeEmployee = (e) => {
+    //     this.setState({
+    //         employee: e.target.value
+    //     })
+    // }
+
+    // onChangeStatus = (e) => {
+    //     this.setState({
+    //         status: e.target.value
+    //     })
+    // }
+
+
 
     //DO NOT DELETE !!!
     // onSubmit(e) {
@@ -154,7 +168,7 @@ const SprintsForms = (props) => {
     // }
 
 
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault();
 
         let checkArray = [];
@@ -168,7 +182,7 @@ const SprintsForms = (props) => {
             title: this.state.title,
             description: this.state.description,
             manager: this.state.manager,
-            employee: this.state.employeeData,
+            employee: this.state.employee,
             startDate: this.state.startDate,
             status: this.state.status,
             employeeData: checkArray.toString()
@@ -180,7 +194,19 @@ const SprintsForms = (props) => {
     }
 
 
+    const employeeOption = employee.map((selectedOption) => {
 
+        return {
+            value: selectedOption,
+            label: selectedOption
+        }
+    })
+
+    const handleChange = selectedOption => {
+
+
+        console.log(selectedOption);
+    };
 
 
 
@@ -188,38 +214,53 @@ const SprintsForms = (props) => {
         <Container>
             <Col md="4" className="mx-auto">
                 <h1>Sprint Form</h1>
-                <Form onSubmit={this.onSubmit}  >
+                <Form onSubmit={onSubmit}  >
 
                     <Form.Group controlId="exampleForm.ControlInput1">
                         <Form.Label>Sprints Name</Form.Label>
                         <Form.Control type="sprints" placeholder="Sprints Name" className="form-control"
-                            value={this.state.title}
-                            onChange={this.onChangeTitle} />
+                            value={title}
+                            onChange={(e) => setTitle(e.target.value)} />
                     </Form.Group>
                     <Form.Group controlId="exampleForm.ControlInput2">
                         <Form.Label>Sprints Details</Form.Label>
                         <Form.Control type="details" placeholder="Sprints Details"
                             className="form-control"
-                            value={this.state.description}
-                            onChange={this.onChangeDescription} />
+                            value={description}
+                            onChange={(e) => setDescription(e.target.value)} />
                     </Form.Group>
 
 
                     <Form.Group controlId="exampleForm.ControlSelect1">
                         <Form.Label>Manager</Form.Label>
                         <Form.Control as="select" custom className="form-control"
-                            value={this.state.manager}
-                            onChange={this.onChangeManager}>
-                            {this.state.employee.map(function (employee) {
+                            value={manager}
+                            onChange={(e) => setManager(e.target.value)}>
+                            {employee.map((option) => {
                                 return <option
-                                    key={employee}
-                                    value={employee}>{employee}
+                                    // key={option}
+                                    value={option}>{option}
                                 </option>;
                             })}
                         </Form.Control>
 
+                        {/* <Select
+                            options={employee}
+                            onChange={(e) => {
+                                setEmployeeData(e.target.value)
+                                console.log(e.target.value)
+                            }}
+                            value={employee.filter(function (option) {
+                                return option.value === selectedEmployee;
+                            })}
+                            label="Single select"
+                        /> */}
+
 
                     </Form.Group>
+
+
+
                     {/* //DO NOT DELETE !!! */}
                     {/* <Form.Group controlId="exampleForm.ControlSelect2">
                             <Form.Label>Employee</Form.Label>
@@ -237,35 +278,37 @@ const SprintsForms = (props) => {
                             </Form.Control> */}
 
 
-
-                    <div class="form-check">
+                    <Form>Employee</Form>
+                    {/* <div class="form-check">
                         <input
                             class="form-check-input"
                             type="checkbox"
                             onChange={(e) => {
-                                this.setState({ employeeData: e.target.value })
+                                setEmployeeData(e.target.value)
                                 console.log(e.target.value)
                             }}
-                        // checked={this.state.employeeData}
                         />
                         <label class="form-check-label" for="defaultCheck1">
-                            {this.state.employee.map(function (employee) {
+                            {employee.map((employee) => {
                                 return <option
                                     key={employee}
                                     value={employee}>{employee}
                                 </option>;
                             })}
                         </label>
+                    </div> */}
+                    <div>
+                        <Select options={employeeOption} onChange={handleChange} isMulti />
                     </div>
-
 
 
                     <Form.Group controlId="exampleForm.ControlSelect3">
                         <Form.Label>Status</Form.Label>
                         <Form.Control as="select" custom defaultValue="Open" className="form-control"
-                            value={this.state.status}
-                            onChange={this.onChangeStatus}>
-                            {this.status.map((options) => {
+                            value={status}
+                            onChange={(e) => setStatus(e.target.value)}
+                        >
+                            {status.map((options) => {
                                 return (
                                     <option>{options}</option>
                                 )
