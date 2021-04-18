347a5ccb601431e45504379d38afd4f25ce4990b
johnniesimpson <johnniesimpson@outlook.com>
2021-04-18 14:58:25 -0400

    Refactoring dashboard components. Scrapping separate DashForm. Moved DashForm content to the Dashboard js. Trying to get the click function to render state info.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,39 +1,119 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Card from "../Cards/Cards"
 import CustomPieChart from "../Charts/PieChart";
 import "./dashboard.css";
 import Container from "../Container/Container";
 import Row from "../Row/Row";
 import Col from "../Col/Col";
-import DashForm from "../Forms/DashForm";
+import Form from 'react-bootstrap/Form';
+import axios from "axios";
 
-const cardData = [
-    {
-        title: "Summary",
-        text: "This is the Summary"
-    },
-    {
-        title: "Employees",
-        text: "Employee 1"
-    },
-    {
-        title: "Managers",
-        text: "Manager 1"
-    },
-    {
-        title: "Timeline",
-        text: "This is our Timeline"
-    }
+// const cardData = [
+//     {
+//         title: "Project Title",
+//         text: "Description of project"
+//     },
+//     {
+//         title: "Employees",
+//         text: "Employee 1"
+//     },
+//     {
+//         title: "Managers",
+//         text: "Manager 1"
+//     },
+//     {
+//         title: "Timeline",
+//         text: "This is our Timeline"
+//     }
 
-]
+// ]
 
 function Dashboard() {
+
+    const [cardData, setCardData] = useState([
+         {
+            title: "Project Title",
+            text: "Project Description"
+        },
+        // {
+        //     title: "Employees",
+        //     text: "Employee 1"
+        // },
+        // {
+        //     title: "Managers",
+        //     text: "Manager 1"
+        // },
+        // {
+        //     title: "Timeline",
+        //     text: "This is our Timeline"
+        // }
+    ]
+    )
+       
+
+    const [sprintTitles, setSprintTitles] = useState([]);
+
+    const [results, setResults] = useState([]);
+
+    const [id, setId] = useState();
+
+    const runAxios = () => {
+        axios.get(`http://localhost:5000/projects/${id}`).then(
+            sprints => {
+                console.log(sprints);
+            }
+        ).catch(error => {
+            console.log(error);
+        })
+    }
+
+    useEffect(
+        () => {
+            axios.get("http://localhost:5000/projects/").then(
+                sprints => {
+                    console.log(sprints.data[0]._id);
+                    setResults(sprints.data);
+                    
+                }
+            ).catch(error => {
+                console.log(error);
+            })
+    }, []);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        const {name, value} = e.target;
+        console.log(e.target.value);
+        setId(e.target.value);
+    }
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        axios.get(`http://localhost:5000/projects/${id}`).then(
+                sprints => {
+                    console.log(sprints.data);
+                    console.log(sprints.data.description)                    
+                }
+            ).catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div className='dashboard'>
             <h1>Dashboard Page</h1>
             <Row>
                 <Col size="3">
-                   <DashForm />
+                <Form id={id}> 
+            <Form.Group>
+                <Form.Label>Your Projects</Form.Label>
+                <Form.Control size="md" as="select" multiple name="id" onChange={handleChange} onClick={runAxios}>
+                   { results.map(sprints => {
+                      return <option value={sprints._id} key={sprints._id}>{sprints.title} </option>
+                    })}
+                </Form.Control> 
+            </Form.Group>
+        </Form>
                 </Col>
                 <Col size="4">
                     <div>{cardData.map((data) => {
