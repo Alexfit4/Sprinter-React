9368a063a3c3b69b6cfc4522845db7f68f04bf27
johnniesimpson <johnniesimpson@outlook.com>
2021-04-24 12:05:02 -0400

    Save before pull

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -19,6 +19,7 @@ function Navbar() {
 				<Link to={window.location.pathname} className="menu-bars">
 					<FaIcons.FaBars onClick={showSidebar} />
 				</Link>
+                <div className="title">Sprinter</div>
 			</div>
             <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
                 <ul className="nav-menu-items" onClick={showSidebar}>
